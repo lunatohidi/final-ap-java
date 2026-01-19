156b4649934eb21f6cbcc34e98d5672b493dab5f
alirezafayazisafa <fayazisafaalireza.aut@gmail.com>
2026-01-19 19:12:18 +0330

    Write User and UserRole in Model.

--- final-ap-java/src/main/java/Model/User.java
+++ final-ap-java/src/main/java/Model/User.java
@@ -1,4 +1,48 @@
 package Model;
 
-public class User {
+public class User implements UserRole {
+    private String username;
+    private String password;
+    private String role;  // مثلا customer یا admin
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public void performRole() {
+        if ("customer".equals(role)) {
+            System.out.println(username + " is performing customer actions.");
+        } else if ("admin".equals(role)) {
+            System.out.println(username + " is performing admin actions.");
+        } else {
+            System.out.println(username + " has an unknown role.");
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }

a55099ebf05f068edad3f61d567234c4c3afa2c5
alirezafayazisafa <fayazisafaalireza.aut@gmail.com>
2026-01-21 14:36:36 +0330

    Write UserProfile in Model.

--- final-ap-java/src/main/java/Model/UserProfile.java
+++ final-ap-java/src/main/java/Model/UserProfile.java
@@ -1,4 +1,28 @@
 package Model;
 
 public class UserProfile {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+    private double walletBalance;
+
+    public UserProfile(String username, String firstName, String lastName, String email, String role, double walletBalance) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+        this.walletBalance = walletBalance;
+    }
+
+    public String getUsername() { return username; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+    public String getRole() { return role; }
+    public double getWalletBalance() { return walletBalance; }
+
+    public void setWalletBalance(double walletBalance) { this.walletBalance = walletBalance; }
 }
